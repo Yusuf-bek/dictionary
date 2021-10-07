5e556f2c12148aa595546f898c5fa12d001198f5
Ogabek <ogabeksherakhmatov@gmail.com>
2021-10-07 10:36:41 +0500

    Skeleton is ready :)

--- nimadir.py
+++ nimadir.py
@@ -1,1 +1,37 @@
-print("Projectni boshladik")
+class Dictionary:
+
+	def add_new_word(self):
+		pass
+
+
+	def show_words(self):
+		pass
+
+
+	def search_word(self):
+		pass
+
+
+	def quit(self):
+		pass
+
+
+	def show_text(self):
+		print("""
+
+		Welcome to dicrionary!
+		_________________________________________
+
+		English - Uzbek dictionary
+
+		Choose:
+
+			1. Add new word to dictionary;
+			2. See words inside the dictionary;
+			3. Search;
+			4. Quit;
+
+
+		_________________________________________
+
+			""")
