97eceb40205d0a6f6dda8c26debacdce3f9797c5
Ogabek <ogabeksherakhmatov@gmail.com>
2021-10-07 15:21:19 +0500

    Step 6. Show words is ready and other changes

--- nimadir.py
+++ nimadir.py
@@ -7,6 +7,7 @@ mydb = mysql.connector.connect(
 			password='123123123',
 			database='words'
 		)
+mycursor = mydb.cursor()
 
 class Dictionary:
 
@@ -14,12 +15,14 @@ class Dictionary:
 		self.new_word_en = None
 		self.new_word_uz = None
 
+
 	def entrance(self):
 		self.show_text()
 		options = '1234'
 		option = input(": ").strip()
 
 		while option not in options:
+			self.clear_everything()
 			self.show_text()
 			option = input("Invalid input. Enter only [1, 2, 3, 4]: ").strip()
 
@@ -33,6 +36,7 @@ class Dictionary:
 			self.quit()
 
 	def add_new_word(self):
+
 		new_word = input("Input new english word: ").strip().lower()
 		while not new_word.isalpha():
 			self.clear_everything()
@@ -46,10 +50,25 @@ class Dictionary:
 			new_word_translate = input('Input translation of word: ').strip().lower()
 
 		self.save_to_database(new_word, new_word_translate)
+		# self.check_word_inside()
 
 
 	def show_words(self):
-		print("Show words")
+		mycursor.execute("select * from table_words")
+		var_show = mycursor.fetchall()
+
+		self.clear_everything()
+
+		print("\n")
+		print("[id]   [English]   [Uzbek]")
+		for i in var_show:
+			print("------------------------")
+			print(f"{i[0]}| {i[1]} - {i[2]}")
+
+
+
+
+
 
 
 	def search_word(self):
@@ -81,15 +100,18 @@ class Dictionary:
 
 
 	def save_to_database(self, new_word_en, new_word_uz):
-		mycursor = mydb.cursor()
 
 		mycursor.execute(f"insert into table_words (english, uzbek) values ('{new_word_en}', '{new_word_uz}')")
 		mydb.commit()
 
 
+
 	@staticmethod
 	def clear_everything():
 		os.system("clear")
+
+
+
 
 
 en_uz = Dictionary()
